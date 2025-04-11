f38ae776972f9a6b717f3e64422bc0880e7199c1
Henrik Andersen <han@rts.dk>
2025-04-11 13:10:00 +0200

    Add base and CSS configuration to Vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: './',
+
+    css: {
+        devSourcemap: true
+    },
+
     build: {
         outDir: 'docs',
     }
